70ff9b438895a41735549f6ca15e3efd17dfe6a1
Alexander Holzmann <alex@ajama.de>
2021-12-29 22:03:10 +0100

    2-22 setting up the course component - also fix createHttpObservable() method (because of deprecated create() method and return type)

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,12 +1,28 @@
 import { Observable } from 'rxjs';
 
 /**
- * createHttpObservable(string) returns an Observable of type Object
+ * createHttpObservable(string) returns an Observable of type any
+ *
+ *     create is deprecated: use new Observable() instead:
+ *     https://stackoverflow.com/questions/55539103/angular-create-is-deprecated-use-new-observable-instead
+ *
+ *     deprecated version:
+ *     export function createHttpObservable(url: string): Observable<Object> {
+ *       return Observable.create(observer => {
+ *         [...]
+ *       }
+ *     }
+ *
+ *     new version: see below
+ *
+ *     see also: what's the correct way to create a typed Observable with RxJS?
+ *               https://stackoverflow.com/questions/58758558/whats-the-correct-way-to-create-a-typed-observable-with-rxjs
  *
  * @param url
  */
-export function createHttpObservable(url: string): Observable<Object> {
-  return Observable.create(observer => {
+
+export function createHttpObservable(url: string): Observable<any> {
+  return new Observable<any>(observer => {
 
     const controller = new AbortController(); // AbortController is part of the fetch API
     const signal = controller.signal;         // signal is provided by the AbortController
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,20 +1,12 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {Course} from "../model/course";
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { Course } from "../model/course";
 import {
-    debounceTime,
-    distinctUntilChanged,
-    startWith,
-    tap,
-    delay,
     map,
-    concatMap,
-    switchMap,
-    withLatestFrom,
-    concatAll, shareReplay
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat} from 'rxjs';
-import {Lesson} from '../model/lesson';
+import { Observable } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { createHttpObservable } from '../common/util';
 
 
 @Component({
@@ -40,7 +32,13 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
         const courseId = this.route.snapshot.params['id'];
 
+        this.course$ = createHttpObservable(`/api/courses/${courseId}`);
 
+        this.lessons$ =
+            createHttpObservable(`/api/lessons?courseId=${courseId}&pageSize=100`)
+              .pipe(
+                  map(res => res['payload'])
+              );
 
     }
 
