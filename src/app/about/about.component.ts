f55a4f867e380c3e014c46f7346c54f2bfbd5124
Alexander Holzmann <alex@ajama.de>
2021-12-29 21:06:48 +0100

    2-21 unsubscription - implementing a cancellable HTTP observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { interval, merge } from 'rxjs';
 import { map } from 'rxjs/operators';
+import {createHttpObservable} from '../common/util';
 
 @Component({
   selector: 'about',
@@ -13,13 +14,13 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
-    const interval1$ = interval(1000);
+      const http$ = createHttpObservable('/api/courses');
 
-    const interval2$ = interval1$.pipe(map(val => 10 * val));
+      const sub = http$.subscribe(console.log);
 
-    const result$ = merge(interval1$, interval2$);
-
-    // result$.subscribe(console.log);
+      // depending on the local machine (cpu speed) timeout has to be increased.
+      // timeout '0' didn't work for me... unsubscribe started too early.
+      setTimeout(() => sub.unsubscribe(), 10);
 
   }
 
--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -8,8 +8,12 @@ import { Observable } from 'rxjs';
 export function createHttpObservable(url: string): Observable<Object> {
   return Observable.create(observer => {
 
+    const controller = new AbortController(); // AbortController is part of the fetch API
+    const signal = controller.signal;         // signal is provided by the AbortController
+                                              //   - signal = true: fetch requests is being cancelled by the browser
+
     // Browser Fetch API
-    fetch(url)
+    fetch(url, { signal })
       .then(response => {
         return response.json();
       })
@@ -20,5 +24,8 @@ export function createHttpObservable(url: string): Observable<Object> {
       .catch(err => {
         observer.error(err);
       });
+
+    return () => controller.abort();
+
   });
 }
